84c0a6777471b9d68d6ea05dd9da05625b4329eb
Dan Smith <68794306+dan-th3-man@users.noreply.github.com>
2025-02-04 19:15:21 +0000

    Update claim-conditions.ts

--- src/constants/claim-conditions.ts
+++ src/constants/claim-conditions.ts
@@ -50,7 +50,6 @@ export const CLAIM_CONDITIONS: ClaimCondition[] = [
   },
   {
     badgeId: "0x716ad1b6222046289c1664825cd9e4caf6253aec",
-    mustOwnBadge: "0x3e9af5c6ae7f7936c629de669a0e3295f3266fb0",
     claimableFrom: new Date("2025-01-29T00:00:00Z"),
     claimableTo: new Date("2025-02-12T23:59:59Z"),
   },
