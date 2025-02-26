bd1aab4f4ec0768c845ec9e96a9ad37b3236040b
Dan Smith <68794306+dan-th3-man@users.noreply.github.com>
2025-02-26 13:16:16 +0000

    added Saint-Brieuc vs PSG: 26/02 badge

--- src/constants/claim-conditions.ts
+++ src/constants/claim-conditions.ts
@@ -36,4 +36,11 @@ export const CLAIM_CONDITIONS: ClaimCondition[] = [
   {
     badgeId: "0xd1b3f1e744d705b8c52faa3c6f769ddbd6ffe9d8",
   },
+  {
+    badgeId: "0x002ff408b13a677f0d5eefe0f5c523f967bd0d5A ",
+    mustOwnBadge: "0x3e9af5c6ae7f7936c629de669a0e3295f3266fb0",
+    claimableFrom: new Date("2025-02-26T00:00:00Z"),
+    claimableTo: new Date("2025-02-26T23:59:59Z"),
+    hide: false
+  }
 ];
