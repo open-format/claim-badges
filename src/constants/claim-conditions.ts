b639d0a211e83c3a042e574a98570de00e04d3f9
Dan Smith <68794306+dan-th3-man@users.noreply.github.com>
2025-04-09 02:10:18 +0100

    PSG vs Birmingham Badge

--- src/constants/claim-conditions.ts
+++ src/constants/claim-conditions.ts
@@ -37,9 +37,9 @@ export const CLAIM_CONDITIONS: ClaimCondition[] = [
     badgeId: "0xd1b3f1e744d705b8c52faa3c6f769ddbd6ffe9d8",
   },
   {
-    badgeId: "0x60179fb3eda9c9837e09b2c9662acfcfe3f8050e",
-    claimableFrom: new Date("2025-04-05T00:00:00Z"),
-    claimableTo: new Date("2025-04-05T23:59:59Z"),
+    badgeId: "0x76dc1f431b622098f4112d5dbc956d384d3c6548",
+    claimableFrom: new Date("2025-04-09T00:00:00Z"),
+    claimableTo: new Date("2025-04-09T23:59:59Z"),
     hide: false,
   }
 ];
